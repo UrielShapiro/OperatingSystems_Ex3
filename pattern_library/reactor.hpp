4926525b3eb22d8973b713881fd195b139686488
Chaim Averbach <Chaymi99@gmail.com>
2024-07-04 21:07:37 +0300

    Added documentation to reactor.hpp

--- pattern_library/reactor.hpp
+++ pattern_library/reactor.hpp
@@ -6,8 +6,19 @@
 #include <mutex>
 #include <vector>
 
+/**
+ * This class implements the reactor design pattern.
+ * The reactor saves a list of file descriptors and handlers.
+ * While running the reactor continuously polls the file descriptors for events.
+ * When an event occurs on one of them (currently only event is information available for reading) the handler is called.
+ */
 class Reactor
 {
+public:
+    /**
+     * The type used as a handler for reactions to file descriptor events.
+     * The function receives as an argument the file descriptor on which the event occured.
+     */
     using Handler = std::function<void (int)>;
 private:
     bool running;
@@ -19,10 +30,28 @@ private:
     std::mutex vectors_mutex;
     void reactor_main();
 public:
+    /**
+     * Creates a new reactor, intially not running, you can add file descriptors to it and use start() to start it.
+     */
     Reactor();
+    /**
+     * Destroys the reactor, calls stop().
+     */
     ~Reactor();
+    /**
+     * Starts the reactor in a separate thread.
+     */
     void start();
+    /**
+     * Stops the reactor, blocks until all currently polled operations are finished.
+     */
     void stop();
+    /**
+     * Adds a new file descriptor to the reactor, when events occur on it handler will be called.
+     */
     bool add_fd(int fd, Handler handler);
+    /**
+     * Removes from the reactor the file descriptor and its handler.
+     */
     bool remove_fd(int fd);
 };
